17b9e4b1be956ead80f98166c07370eb433c8558
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-06-06 22:13:30 +0800

    chore: add and setup s3 storage

--- nitro.config.ts
+++ nitro.config.ts
@@ -8,5 +8,13 @@ export default defineConfig({
       driver: 'redis',
       url: process.env.REDIS_URL,
     },
+    s3: {
+      driver: 's3',
+      accessKeyId: process.env.S3_ACCESS_KEY_ID,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+      endpoint: process.env.S3_ENDPOINT,
+      bucket: process.env.S3_BUCKET,
+      region: process.env.S3_REGION,
+    },
   },
 });
