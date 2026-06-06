310ec7da472da1fe1f5a8416487e46bafef2f1c0
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-06-06 22:03:47 +0800

    chore: add and setup redis cache

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,4 +3,10 @@ import { defineConfig } from 'nitro';
 export default defineConfig({
   compatibilityDate: '2026-06-06',
   serverDir: './server',
+  storage: {
+    cache: {
+      driver: 'redis',
+      url: process.env.REDIS_URL,
+    },
+  },
 });
